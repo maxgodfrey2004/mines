1a4aa42965862d77aeafbe91a898813327a7ba16
Max Godfrey <1020167@student.ccgs.wa.edu.au>
2020-01-19 17:05:35 +0800

    Bump copyright

--- game/renderer.go
+++ game/renderer.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Max Godfrey
+// Copyright 2020 Max Godfrey
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
